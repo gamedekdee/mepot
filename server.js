adde791ac6a8465ece165393b24d5bdeb55cd979
gamedekdee <gamedekdee05@gmail.com>
2025-07-30 14:37:08 +0700

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -173,4 +173,6 @@ app.post('/api/reset-password', async (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('🚀 Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
